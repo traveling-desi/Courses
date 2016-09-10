a235f246ee978edd6ef8bde31fbf36a99fb07b3d
nik <sarpotd@yahoo.com>
2016-09-09 17:43:50 -0700

    .

---
+++ Coursera/Finance/Computational Investing/HW3/hw3.py
@@ -0,0 +1,113 @@
+import sys
+import re
+import pandas 
+from qstkutil import DataAccess as da
+import numpy as np
+import math
+import copy
+import qstkutil.qsdateutil as du
+import datetime as dt
+import qstkutil.DataAccess as da
+import qstkutil.tsutil as tsu
+import qstkstudy.EventProfiler as ep
+
+verbose =0
+closefield = "close"
+portfolio = {}
+
+cmd_line = sys.argv
+portfolio_value = 0
+portfolio["cash"] = int(cmd_line[1],10)
+ord_file = cmd_line[2]
+val_file =cmd_line[3]
+#print ord_file
+#print val_file
+
+lines = []
+f = open(ord_file, "r")
+try:
+	for line in f:
+		lines.append(re.sub('[\n]','',line))
+finally:
+	f.close()
+
+
+symbols = []
+timeofday=dt.timedelta(hours=16)
+dataobj = da.DataAccess('Yahoo')
+if verbose:
+	print __name__ + " reading data"
+
+for i in range (len(lines)):
+	order = lines[i].split(',')
+	print lines[i]
+	print order[0]
+	print order[1]
+	print order[2]
+	print order[3]
+	print order[4]
+	print order[5]
+	if i == 0:
+		startday = dt.date(int(order[0],10),int(order[1],10),int(order[2],10))
+		print startday
+	if i == len(lines) - 1:
+		endday = dt.date(int(order[0],10),int(order[1],10),int(order[2],10))
+		print endday
+	if (order[3] in symbols) == False:
+		symbols.append(order[3])
+	print symbols
+
+for sym in symbols:
+	portfolio[sym] = 0
+
+
+timestamps = du.getNYSEdays(startday,endday,timeofday)
+#print "TIME"
+print timestamps
+close = dataobj.get_data(timestamps, symbols, closefield)
+#close = (close.fillna(method='ffill')).fillna(method='backfill')
+
+#print close
+
+for sym in symbols:
+       for time in timestamps:
+		print time
+                print close[sym][time]
+j = 0
+for i in range (len(timestamps)):	
+	order = lines[j].split(',')
+	day_time = dt.datetime(int(order[0],10),int(order[1],10),int(order[2],10),16,0)
+	portfolio_value = 0
+	for sym in portfolio:
+		if sym == "cash" :
+			portfolio_value = portfolio_value + portfolio[sym]
+		else:
+			portfolio_value = portfolio_value + close[sym][timestamps[i]]*portfolio[sym]
+			
+	print timestamps[i], day_time, portfolio, portfolio_value
+		
+	if(timestamps[i] == day_time):
+		while (timestamps[i] == day_time):
+			price = close[order[3]][day_time]
+			cash_change = int(order[5],10)*price
+			if (order[4] == "Buy"):
+				portfolio["cash"] = portfolio["cash"] - cash_change
+				portfolio[order[3]] = portfolio[order[3]] + int(order[5],10)
+			else:
+				portfolio["cash"] = portfolio["cash"] + cash_change
+				portfolio[order[3]] = portfolio[order[3]] - int(order[5],10)
+	
+			print timestamps[i], day_time, portfolio
+			j = j+1
+			order = lines[j].split(',')
+			day_time = dt.datetime(int(order[0],10),int(order[1],10),int(order[2],10),16,0)
+
+	
+		
+		
+
+# Reading the Data
+
+#print lines[0][0:10]
+#line_date = dt.datetime.strptime('2011,01,23','%Y,%m,%d').date()
+
---
+++ Coursera/Finance/Computational Investing/HW4/hw2.py
@@ -0,0 +1,107 @@
+'''
+(c) 2011, 2012 Georgia Tech Research Corporation
+This source code is released under the New BSD license.  Please see
+http://wiki.quantsoftware.org/index.php?title=QSTK_License
+for license details.
+
+Created on March, 5, 2012
+
+@author: Sourabh Bajaj
+@contact: sourabhbajaj90@gmail.com
+@summary: Event Profiler Tutorial
+'''
+
+
+import pandas 
+from qstkutil import DataAccess as da
+import numpy as np
+import math
+import copy
+import qstkutil.qsdateutil as du
+import datetime as dt
+import qstkutil.DataAccess as da
+import qstkutil.tsutil as tsu
+import qstkstudy.EventProfiler as ep
+
+"""
+Accepts a list of symbols along with start and end date
+Returns the Event Matrix which is a pandas Datamatrix
+Event matrix has the following structure :
+    |IBM |GOOG|XOM |MSFT| GS | JP |
+(d1)|nan |nan | 1  |nan |nan | 1  |
+(d2)|nan | 1  |nan |nan |nan |nan |
+(d3)| 1  |nan | 1  |nan | 1  |nan |
+(d4)|nan |  1 |nan | 1  |nan |nan |
+...................................
+...................................
+Also, d1 = start date
+nan = no information about any event.
+1 = status bit(positively confirms the event occurence)
+"""
+
+# Get the data from the data store
+storename = "Yahoo" # get data from our daily prices source
+# Available field names: open, close, high, low, close, actual_close, volume
+closefield = "actual_close"
+volumefield = "volume"
+window = 10
+
+def findEvents(symbols, startday,endday, marketSymbol, ord_file, verbose=False):
+
+	# Reading the Data for the list of Symbols.	
+	timeofday=dt.timedelta(hours=16)
+	timestamps = du.getNYSEdays(startday,endday,timeofday)
+	dataobj = da.DataAccess('Yahoo')
+	if verbose:
+            print __name__ + " reading data"
+	# Reading the Data
+	close = dataobj.get_data(timestamps, symbols, closefield)
+	
+	# Completing the Data - Removing the NaN values from the Matrix
+	close = (close.fillna(method='ffill')).fillna(method='backfill')
+
+	np_eventmat = copy.deepcopy(close)
+	for sym in symbols:
+		for time in timestamps:
+			np_eventmat[sym][time]=np.NAN
+
+	if verbose:
+            print __name__ + " finding events"
+
+	# Generating the Event Matrix
+
+	f = open(ord_file, "w")
+	try:
+		for symbol in symbols:
+	    		for i in range(1,len(close[symbol])):
+	        		if close[symbol][i-1] >= 7 and close[symbol][i] < 7 : # Event
+             				np_eventmat[symbol][i] = 1.0  #overwriting by the bit, marking the event
+					s = str(timestamps[i]) + "," + (str(symbol) + ",BUY," + "100")
+					f.write(s+'\n')
+					if i+5 > len(timestamps):
+						i = len(timestamps)-6
+					s = str(timestamps[i+5]) + "," + (str(symbol) + ",SELL," + "100")
+					f.write(s+'\n')
+	finally:
+        	f.close()
+
+	return np_eventmat
+
+
+#################################################
+################ MAIN CODE ######################
+#################################################
+
+orders_file = "new_orders.csv"
+symbols = np.loadtxt('sp5002012.txt',dtype='S10',comments='#', skiprows=1)
+# You might get a message about some files being missing, don't worry about it.
+
+startday = dt.datetime(2008,1,1)
+endday = dt.datetime(2009,12,31)
+eventMatrix = findEvents(symbols,startday,endday,marketSymbol='SPY',ord_file = orders_file,verbose=True)
+
+eventProfiler = ep.EventProfiler(eventMatrix,startday,endday,lookback_days=20,lookforward_days=20,verbose=True)
+
+eventProfiler.study(filename="MyEventStudy.pdf",plotErrorBars=True,plotMarketNeutral=True,plotEvents=False,marketSymbol='SPY')
+
+
